dd0ae3f80ac0c49d126dcea36ddbb8cac9457ebc
lpoliveiraneto <leonidespires@gmail.com>
2021-03-20 13:53:45 -0300

    catalogo online

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return(
+    <div>
+      <Head>
+        <title>Cardápio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+       <h1>Tá saindo o monstro</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
